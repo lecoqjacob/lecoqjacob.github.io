508d11642454c19714ee17d0d9c149a1f0ff24a5
Jacob LeCoq <lecoqjacob@gmail.com>
2022-11-01 18:16:44 -0500

    start port to tailwindcss

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+// prettier.config.js
+module.exports = {
+  tailwindConfig: "./tailwind.config.cjs",
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2,
+  bracketSameLine: true,
+};
